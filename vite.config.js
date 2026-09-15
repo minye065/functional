e479da35fd2ba73d210cfc348b4eec630d5bdd44
minye065 <minye065@fusdk12.net>
2026-09-14 21:52:36 -0700

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "poseTool",
+  build: { outDir: "../dist" },
   base: process.env.VITE_BASE_PATH || "/functional/",
 });
